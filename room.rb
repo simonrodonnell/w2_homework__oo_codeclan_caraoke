97fa247a909811c985b8cfeec0ee11bcc9b04bdd
Simon O'Donnell <simonrodonnell@yahoo.co.uk>
2019-09-20 13:33:31 +0100

    Checked Guest in & out

--- room.rb
+++ room.rb
@@ -15,4 +15,12 @@ class Room
     return @guests
   end
 
+  def check_guest_in(new_guest)
+    @guests << new_guest
+  end
+
+  def check_guest_out(guest)
+    @guests.delete(guest)
+  end
+
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -12,6 +12,8 @@ class TestRoom < Minitest::Test
     @song3 = Song.new("Smells Like Teen Spirit")
     @song4 = Song.new("Rapper's Delight")
 
+    @new_song = Song.new("Paranoid")
+
     @songs = [@song1, @song2, @song3, @song4]
 
     @guest1 = Guest.new("Frank", 50, @song3)
@@ -35,6 +37,16 @@ class TestRoom < Minitest::Test
 
   def test_room_has_capacity
     assert_equal(3, @room.capacity)
+  end
+
+  def test_check_guest_in
+    @room.check_guest_in(@new_guest)
+    assert_equal(4, @room.guests().length())
+  end
+
+  def test_check_guest_out
+    @room.check_guest_out(@guest3)
+    assert_equal(2, @room.guests().length())
   end
 
 end
